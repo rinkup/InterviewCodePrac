d9217008a6c1219fe57a00f71c2c2c0f34b01717
Rinku Pandey <rinku.pandey8889@gmail.com>
2019-09-02 08:28:25 -0700

    0.2

--- uniqueSubstring.php
+++ uniqueSubstring.php
@@ -5,23 +5,24 @@
 //	Given "abcabcbb", the answer is "abc", which the length is 3.
 
 function uniqueSubstring($inputString){
-	$inputStringA = str_split($inputString);
 	$subString = '';
-	$out = [];
+	$tmpLongestSting = '';
+	$longestString = '';
+	$inputStringA = str_split($inputString);
 	for($i=0; $i<count($inputStringA); $i++){
 		$index = strpos($subString, $inputStringA[$i]); // Return false if NO character found
 		if( ($index === false) || (strlen($subString) == 1) ){
 			$subString = $subString.$inputStringA[$i];
-			echo $subString ."    ".$inputStringA[$i]."\n";
 		}else{
-			array_push($out,$subString);
+			if(strlen($subString) > strlen($longestString)){
+				$longestString = $subString;
+			}
 			$subString = $inputStringA[$i];
 		}
 //	echo $inputStringA[$i];
 	}	
-	array_push($out,$subString);
-	return $out;	
+	return $longestString.strlen($longestString);	
 }
 
-var_export(uniqueSubstring('abcabcbb'));
+var_export(uniqueSubstring('abcdajkesplmnzxbcabcdeabcdbcdefsre'));
 ?>
