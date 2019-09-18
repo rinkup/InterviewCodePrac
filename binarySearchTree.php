3be578897b4811f6f7121a73556f2ceb62b8c3e1
Rinku Pandey <rinku.pandey8889@gmail.com>
2019-09-17 21:53:01 -0700

    0.2

--- binarySearchTree.php
+++ binarySearchTree.php
@@ -2,6 +2,10 @@
 function binarySearchTree($findElement, $input, $pointer){
 	$size = count($input);
 	$mid = floor($size/2);
+	// Element not found
+	if($size==1 && $input[0]!=$findElement ){
+		return "Element not found";
+	}
 	if($input[$mid] < $findElement){
 		$input = array_splice($input, $mid, $size);
 		return binarySearchTree($findElement, $input, ($pointer+$mid));
@@ -12,8 +16,8 @@ function binarySearchTree($findElement, $input, $pointer){
 		return $pointer+$mid;
 	}
 }
-$input = [4, 6, 27, 43, 54, 85, 105, 127, 153, 223];
-$findElement = 153;
+$input = [4, 6, 27, 43, 54, 63, 85, 105, 127, 135, 153, 176, 201, 223];
+$findElement = 223;
 $findElementIndex = binarySearchTree($findElement, $input, 0);
 echo $findElementIndex."\n";
 ?>
